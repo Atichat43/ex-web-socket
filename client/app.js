ddc416d79b6f29b8dd11648615e2e2b80821c7d3
Atichat Lappanopakon <boss_atln@outlook.com>
2019-08-25 10:12:20 +0700

    Init & Setup project

---
+++ client/app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="app">
+      <header className="app-header" />
+      <p> Application </p>
+    </div>
+  )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, 'client', 'index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"development"',
+      },
+    }),
+  ],
+  devServer: {
+    port: 3001,
+    contentBase: path.resolve(__dirname, 'public'),
+  },
+  devtool: 'cheap-eval-source-map',
+}
